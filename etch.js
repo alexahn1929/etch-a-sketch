c3fc9ceb9fa884644df9dc5981e94c3bc0732079
alexahn1929 <121832249+alexahn1929@users.noreply.github.com>
2023-04-14 17:31:12 -0400

    Add grid resizing button
    Add a button which prompts the user to set a new grid size.

--- etch.js
+++ etch.js
@@ -1,24 +1,42 @@
-const NUM_ROWS = 16;
-const NUM_COLS = 16;
+const DEFAULT_DIMENSION = 16;
 
-const container = document.querySelector("#container");
-for (let i = 0; i < NUM_ROWS; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < NUM_COLS; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", () => square.style.backgroundColor = "black");
-        row.appendChild(square);
+function makeGrid(container, sideLength) {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
     }
-    container.appendChild(row);
+    for (let i = 0; i < sideLength; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < sideLength; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", () => square.style.backgroundColor = "black");
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
 }
 
-let btn = document.querySelector("button");
-btn.addEventListener("click", () => {
-    for (const r of container.children) {
+function getDimension() {
+    let dim;
+    while(isNaN(dim = prompt("Grid dimension to resize to? (max 100)")) || +dim > 100) {}
+    return +dim;
+}
+
+const grid = document.querySelector("#container");
+makeGrid(grid, DEFAULT_DIMENSION);
+
+let eraseBtn = document.querySelector("#erase");
+    eraseBtn.addEventListener("click", () => {
+    for (const r of grid.children) {
         for (const sq of r.children) {
             sq.style.backgroundColor = "white";
         }
     }
 });
+
+let resizeBtn = document.querySelector("#resize");
+resizeBtn.addEventListener("click", () => {
+    makeGrid(grid, getDimension());
+});
+
